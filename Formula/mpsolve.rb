8cd6ece63f12392e046099c94822695235c8174b
Mahrud Sayrafi <3147718+mahrud@users.noreply.github.com>
2021-01-04 11:32:40 +0000

    mpsolve: add 3.2.1 bottle.

--- Formula/mpsolve.rb
+++ Formula/mpsolve.rb
@@ -5,6 +5,13 @@ class Mpsolve < Formula
   sha256 "3d11428ae9ab2e020f24cabfbcd9e4d9b22ec572cf70af0d44fe8dae1d51e78e"
   license "GPL-3.0-only"
 
+  bottle do
+    root_url "https://github.com/mahrud/homebrew-tap/releases/download/mpsolve-3.2.1"
+    cellar :any
+    sha256 "5b0d39bfe221ffa181e198deb8e01429ddcee7ae4624bc3def014415628377b1" => :catalina
+    sha256 "dcc44b785b1c3a626735842c7ad793050d37432878ddccab055390896cbc082c" => :x86_64_linux
+  end
+
   depends_on "autoconf" => :build
   depends_on "automake" => :build
   depends_on "libtool" => :build
