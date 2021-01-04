7795eb566e8f73f9c1cd2bf2f3b84e51ab6b97b4
Mahrud Sayrafi <3147718+mahrud@users.noreply.github.com>
2021-01-04 19:19:31 +0000

    mathicgb: add 1.0 bottle.

--- Formula/mathicgb.rb
+++ Formula/mathicgb.rb
@@ -5,6 +5,13 @@ class Mathicgb < Formula
   version "1.0"
   license "GPL-2.0-or-later"
 
+  bottle do
+    root_url "https://github.com/mahrud/homebrew-tap/releases/download/mathicgb-1.0"
+    cellar :any_skip_relocation
+    sha256 "334f34d4695ddef2bd653e9a9636c2602f268b8f9711982b0ddccb542ce99a36" => :catalina
+    sha256 "6c54d1e3d51bbbcb6c822ff20bd75025e373c34fc8bce6f7bf6619f3b597ea11" => :x86_64_linux
+  end
+
   option "without-mgb", "don't build mgb"
 
   depends_on "cmake" => :build
