e5d63a6b208b8b3708ce5cd4ca03418a21b290e4
Mahrud Sayrafi <3147718+mahrud@users.noreply.github.com>
2021-01-04 17:47:52 +0000

    mathic: add 1.0 bottle.

--- Formula/mathic.rb
+++ Formula/mathic.rb
@@ -5,6 +5,13 @@ class Mathic < Formula
   version "1.0"
   license "LGPL-2.0-or-later"
 
+  bottle do
+    root_url "https://github.com/mahrud/homebrew-tap/releases/download/mathic-1.0"
+    cellar :any_skip_relocation
+    sha256 "3057ed79af4aa8b73d1c7253084fa36dc91aaff36103d3971b81aad2cf989d93" => :catalina
+    sha256 "37866efbbb8f72a6fd9f522b0bd0f2d50c735dd704274af6503a4d020e782591" => :x86_64_linux
+  end
+
   depends_on "cmake" => :build
 
   depends_on "memtailor"
