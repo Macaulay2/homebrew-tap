4eeab5d7ba4cda95000524475ee041298770290a
Mahrud Sayrafi <3147718+mahrud@users.noreply.github.com>
2021-01-05 04:26:32 +0000

    cohomcalg: add 0.32 bottle.

--- Formula/cohomcalg.rb
+++ Formula/cohomcalg.rb
@@ -5,6 +5,13 @@ class Cohomcalg < Formula
   sha256 "367c52b99c0b0a4794b215181439bf54abe4998872d3ef25d793bc13c4d40e42"
   license "GPL-3.0-only"
 
+  bottle do
+    root_url "https://github.com/mahrud/homebrew-tap/releases/download/cohomcalg-0.32"
+    cellar :any_skip_relocation
+    sha256 "e78b6986b83f2b03c6c287aa94a77a374236238292cd81ad20cb1363fe48903d" => :catalina
+    sha256 "9beeba1639ebbc8201c2aeb18ebca2202b8137502be52b5b6456264be7e2f1bc" => :x86_64_linux
+  end
+
   def install
     ENV.cxx11
     system "make", "prefix=#{prefix}",
