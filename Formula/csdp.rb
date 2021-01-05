707ee5f0acace82d7c681868baf8ff15eeb5a514
Mahrud Sayrafi <mahrud@berkeley.edu>
2021-01-04 19:04:40 -0600

    csdp: revision bump

--- Formula/csdp.rb
+++ Formula/csdp.rb
@@ -4,12 +4,7 @@ class Csdp < Formula
   url "https://github.com/coin-or/Csdp/archive/releases/6.2.0.tar.gz"
   sha256 "3d341974af1f8ed70e1a37cc896e7ae4a513375875e5b46db8e8f38b7680b32f"
   license "EPL-2.0"
-
-  bottle do
-    root_url "https://github.com/mahrud/homebrew-tap/releases/download/csdp-6.2.0"
-    cellar :any_skip_relocation
-    sha256 "5a0accab8c702cde119e5c9444a5043bee73a5de7f12b10ee9380930f2a5b982" => :x86_64_linux
-  end
+  revision 2
 
   depends_on "libomp" if OS.mac?
   depends_on "lapack" unless OS.mac?
@@ -17,11 +12,12 @@ class Csdp < Formula
   # patch for compatibility with macOS
   patch do
     url "https://raw.githubusercontent.com/Macaulay2/M2/1f99f71a1308318679412de7f20e940b05f80be6/M2/libraries/csdp/patch-6.2.0"
+    sha256 "e836252c67056e5bc755d24d7a43ccad653139fe8b0b1d2dba064f68da41791c"
   end
 
   def install
-    # inreplace "Makefile", "/usr/local", "$(prefix)"
-
+    ENV["HOMEBREW_VERBOSE"] = "on"
+    ENV["HOMEBREW_DEBUG"] = "on"
     if OS.mac?
       libomp = Formula["libomp"]
       ENV["OpenMP_C_FLAGS"] = "-Xpreprocessor\ -fopenmp\ -I#{libomp.opt_include}"
@@ -33,6 +29,7 @@ class Csdp < Formula
     end
 
     mkdir bin
+    mv "INSTALL", "INSTALL.txt" # the filename confuses `make install`
     system "make",
            "CC=#{ENV.cc} ${OpenMP_C_FLAGS} ${CFLAGS}",
            "LDLIBS=${OpenMP_C_LDLIBS}",
