d0ec58d4b989e00fd1bb33e3006fd3255a37a608
Mahrud Sayrafi <mahrud@berkeley.edu>
2021-01-04 21:53:47 -0600

    frobby: revision bump
    Closes #21.
    
    Signed-off-by: Mahrud Sayrafi <3147718+mahrud@users.noreply.github.com>

--- Formula/frobby.rb
+++ Formula/frobby.rb
@@ -1,9 +1,10 @@
 class Frobby < Formula
   desc "Computations With Monomial Ideals"
   homepage "https://www.broune.com/frobby/"
-  url "https://github.com/Macaulay2/frobby.git", using: :git, branch: "master"
+  url "https://github.com/Macaulay2/frobby.git", using: :git, branch: "Macaulay2-patches"
   version "0.9.1"
   license "GPL-2.0-only"
+  revision 1
 
   bottle do
     root_url "https://github.com/mahrud/homebrew-tap/releases/download/frobby-0.9.1"
@@ -12,14 +13,19 @@ class Frobby < Formula
     sha256 "2af401562c2b3b2eda1c04ef5ccfe4d62fa1b6ef76358c8fad4ea30d3c973bef" => :x86_64_linux
   end
 
+  depends_on "binutils" => :build
   depends_on "gmp"
 
   def install
-    system "make", "prefix=#{prefix}", "BIN_INSTALL_DIR=#{bin}",
+    ENV.cxx11
+    system "make", "bin/libfrobby.a", # "bin/frobby", "BIN_INSTALL_DIR=#{bin}",
            "GMP_INC_DIR=#{Formula["gmp"].include}",
            "LDFLAGS=-L#{Formula["gmp"].lib}",
-           "CXXFLAGS=-std=gnu++11"
-    system "make", "install", "DESTDIR=#{prefix}"
+           "CXXFLAGS=-std=gnu++11",
+           "prefix=#{prefix}",
+           "CXX=#{ENV.cxx}",
+           "RANLIB=ranlib"
+    lib.install "bin/libfrobby.a"
   end
 
   test do
