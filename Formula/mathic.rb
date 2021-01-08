4d245c7ac53abb770005369c9a0d2be77cecab8c
Mahrud Sayrafi <3147718+mahrud@users.noreply.github.com>
2021-01-08 01:23:17 +0000

    mathic: update 1.0_2 bottle.

--- Formula/mathic.rb
+++ Formula/mathic.rb
@@ -7,10 +7,10 @@ class Mathic < Formula
   revision 2
 
   bottle do
-    root_url "https://github.com/mahrud/homebrew-tap/releases/download/mathic-1.0_1"
+    root_url "https://github.com/mahrud/homebrew-tap/releases/download/mathic-1.0_2"
     cellar :any_skip_relocation
-    sha256 "cbbef5fb4be900a16916e7cd305ebdcca141d4201118031b1481c0060b405419" => :catalina
-    sha256 "150d6cd52bdd8bd3e332e765e0a609414695d160de6b6ed18538958e8747642d" => :x86_64_linux
+    sha256 "2e7102b430e1fac56282c520bce2cef6441f66572494e46a916523dab6e0a70a" => :catalina
+    sha256 "309b6aa204cbc970c1acae554cf32b16e6d4eb53aefef72210e2cdce0abd8d08" => :x86_64_linux
   end
 
   unless OS.mac?
