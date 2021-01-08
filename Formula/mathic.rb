03195ef7259bae2b68454214823d15691cbc5b06
Mahrud Sayrafi <mahrud@berkeley.edu>
2021-01-07 19:03:41 -0600

    mathic: revision bump

--- Formula/mathic.rb
+++ Formula/mathic.rb
@@ -4,13 +4,19 @@ class Mathic < Formula
   url "https://github.com/Macaulay2/mathic.git", using: :git, branch: "master"
   version "1.0"
   license "LGPL-2.0-or-later"
-  revision 1
+  revision 2
 
   bottle do
     root_url "https://github.com/mahrud/homebrew-tap/releases/download/mathic-1.0_1"
     cellar :any_skip_relocation
     sha256 "cbbef5fb4be900a16916e7cd305ebdcca141d4201118031b1481c0060b405419" => :catalina
     sha256 "150d6cd52bdd8bd3e332e765e0a609414695d160de6b6ed18538958e8747642d" => :x86_64_linux
+  end
+
+  unless OS.mac?
+    fails_with gcc: "4"
+    fails_with gcc: "5"
+    depends_on "gcc@9" => :build
   end
 
   depends_on "cmake" => :build
