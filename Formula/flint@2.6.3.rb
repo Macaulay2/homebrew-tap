60d693435af7a915f250175cfef479d0a9fbbe88
Mahrud Sayrafi <mahrud@berkeley.edu>
2021-01-08 13:15:11 -0600

    flint@2.6.3: revision bump
    Closes #37.
    
    Signed-off-by: Mahrud Sayrafi <3147718+mahrud@users.noreply.github.com>

--- Formula/flint@2.6.3.rb
+++ Formula/flint@2.6.3.rb
@@ -4,12 +4,19 @@ class FlintAT263 < Formula
   url "https://flintlib.org/flint-2.6.3.tar.gz"
   sha256 "ce1a750a01fa53715cad934856d4b2ed76f1d1520bac0527ace7d5b53e342ee3"
   license "LGPL-2.1-or-later"
+  revision 1
   head "https://github.com/wbhart/flint2.git", branch: "trunk"
 
   bottle do
     root_url "https://github.com/mahrud/homebrew-tap/releases/download/flint@2.6.3-2.6.3"
     sha256 "36f0d62611113c0305e20a5f48e1d335001d9e5aea5002e5a7233ef529845dcf" => :catalina
     sha256 "99c1eca00be8ba0a92f55952eb53b6ac0b7d1c45804ad7f2f7c8a923665ea892" => :x86_64_linux
+  end
+
+  unless OS.mac?
+    fails_with gcc: "4"
+    fails_with gcc: "5"
+    depends_on "gcc@9" => :build
   end
 
   depends_on "gmp"
