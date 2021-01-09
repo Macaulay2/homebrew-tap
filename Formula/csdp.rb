5c185eb36ad99cde501c004877d372986eb0428a
Mahrud Sayrafi <mahrud@berkeley.edu>
2021-01-09 02:26:39 -0600

    csdp: revision bump

--- Formula/csdp.rb
+++ Formula/csdp.rb
@@ -4,14 +4,7 @@ class Csdp < Formula
   url "https://github.com/coin-or/Csdp/archive/releases/6.2.0.tar.gz"
   sha256 "3d341974af1f8ed70e1a37cc896e7ae4a513375875e5b46db8e8f38b7680b32f"
   license "EPL-2.0"
-  revision 5
-
-  bottle do
-    root_url "https://github.com/mahrud/homebrew-tap/releases/download/csdp-6.2.0_4"
-    cellar :any_skip_relocation
-    sha256 "724a1fd6962dfac51604b88a699bde943da47607ada374f34befa35e868989a8" => :catalina
-    sha256 "e5ecdc3f03f8915a67fb34217e2fa8c154c9de1a4b48656641a6c0350354c8b1" => :x86_64_linux
-  end
+  revision 6
 
   depends_on "libomp" if OS.mac?
   depends_on "openblas@0.3.13" unless OS.mac?
