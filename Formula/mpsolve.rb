fbb068d41781a58795e3cd2b1446c15db7214e7b
Mahrud Sayrafi <mahrud@berkeley.edu>
2021-05-24 09:34:20 -0500

    mpsolve: revision bump

--- Formula/mpsolve.rb
+++ Formula/mpsolve.rb
@@ -4,7 +4,7 @@ class Mpsolve < Formula
   url "https://numpi.dm.unipi.it/_media/software/mpsolve/mpsolve-3.2.1.tar.gz"
   sha256 "3d11428ae9ab2e020f24cabfbcd9e4d9b22ec572cf70af0d44fe8dae1d51e78e"
   license "GPL-3.0-only"
-  revision 2
+  revision 3
 
   bottle do
     root_url "https://github.com/Macaulay2/homebrew-tap/releases/download/mpsolve-3.2.1_2"
@@ -29,6 +29,12 @@ class Mpsolve < Formula
   patch do
     url "https://raw.githubusercontent.com/Macaulay2/M2/35c3e5e5f03cb2e60baa8e69f8109afcdb5fdc7b/M2/libraries/mpsolve/patch-3.2.1"
     sha256 "ba5b6064c8a3e9d1894d764aacebe5a623daf1487c7cc44207599df1759036d7"
+  end
+
+  # remove on next release; see https://github.com/robol/MPSolve/issues/27
+  patch do
+    url "https://github.com/robol/MPSolve/commit/3a890878239717e1d5d23f574e4c0073a7249f7a.patch?full_index=1"
+    sha256 "b2c5e037bed14568d3692cf7270428614f2766bcaf0b2fb06a7f178497671efa"
   end
 
   def install
