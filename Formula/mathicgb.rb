400beaa9bf7a63b6e4693ef66299221c694a021c
Mahrud Sayrafi <mahrud@berkeley.edu>
2021-06-04 11:54:39 -0500

    mathicgb: revision bump
    Closes #81.
    
    Signed-off-by: Mahrud Sayrafi <mahrud@umn.edu>

--- Formula/mathicgb.rb
+++ Formula/mathicgb.rb
@@ -1,10 +1,10 @@
 class Mathicgb < Formula
   desc "Compute (signature) Groebner bases using the fast datastructures from mathic"
   homepage "https://github.com/Macaulay2/mathicgb"
-  url "https://github.com/Macaulay2/mathicgb.git", using: :git, branch: "master"
+  url "https://github.com/mahrud/mathicgb.git", using: :git, branch: "master"
   version "1.0"
   license "GPL-2.0-or-later"
-  revision 6
+  revision 7
 
   bottle do
     root_url "https://github.com/Macaulay2/homebrew-tap/releases/download/mathicgb-1.0_6"
@@ -24,7 +24,7 @@ class Mathicgb < Formula
   depends_on "mathic"
   depends_on "memtailor"
 
-  depends_on "tbb" => :optional
+  depends_on "tbb@2020_u3" => :recommended
 
   def install
     ENV.cxx11
@@ -32,7 +32,7 @@ class Mathicgb < Formula
     args << "-DBUILD_TESTING=off"
     args << "-DCMAKE_PREFIX_PATH=#{Formula["memtailor"].prefix};#{Formula["mathic"].prefix}"
     args << "-Denable_mgb=off" if build.without?("mgb")
-    args << "-Dwith_tbb=on" << "-DTBB_ROOT_DIR=#{Formula["tbb"].prefix}" if build.with?("tbb")
+    args << "-Dwith_tbb=on" << "-DTBB_ROOT_DIR=#{Formula["tbb@2020_u3"].prefix}" if build.with?("tbb@2020_u3")
     system "cmake", ".", *args
     system "make", "install"
   end
