e9d77584a97eeeb2c8f905db4bd3b024a26d505f
Mahrud Sayrafi <mahrud@berkeley.edu>
2021-10-16 10:32:46 -0500

    csdp: revision bump

--- Formula/csdp.rb
+++ Formula/csdp.rb
@@ -4,7 +4,7 @@ class Csdp < Formula
   url "https://github.com/coin-or/Csdp/archive/releases/6.2.0.tar.gz"
   sha256 "3d341974af1f8ed70e1a37cc896e7ae4a513375875e5b46db8e8f38b7680b32f"
   license "EPL-2.0"
-  revision 7
+  revision 8
 
   bottle do
     root_url "https://github.com/Macaulay2/homebrew-tap/releases/download/csdp-6.2.0_7"
@@ -27,6 +27,8 @@ class Csdp < Formula
       ENV["OpenMP_C_FLAGS"] = "-Xpreprocessor\ -fopenmp\ -I#{libomp.opt_include}"
       ENV["OpenMP_C_LDLIBS"] = "#{libomp.opt_lib}/libomp.a"
       ENV["LA_LIBRARIES"] = "-framework Accelerate"
+      # see https://github.com/Macaulay2/homebrew-tap/issues/103#issuecomment-944225248
+      inreplace "Makefile", /-ansi/, ""
     else
       ENV["OpenMP_C_FLAGS"] = "-fopenmp"
       ENV["LA_LIBRARIES"] = "-lopenblas"
