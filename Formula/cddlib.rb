c4c1971922b12681335f5b3cdb5285d8e39652c6
Mahrud Sayrafi <mahrud@berkeley.edu>
2021-12-09 18:55:56 -0600

    cddlib: revision bump

--- Formula/cddlib.rb
+++ Formula/cddlib.rb
@@ -4,6 +4,7 @@ class Cddlib < Formula
   url "https://github.com/cddlib/cddlib/releases/download/0.94m/cddlib-0.94m.tar.gz"
   sha256 "70dffdb3369b8704dc75428a1b3c42ab9047b81ce039f12f427e2eb2b1b0dee2"
   license "GPL-2.0-or-later"
+  revision 1
   version_scheme 1
 
   bottle do
