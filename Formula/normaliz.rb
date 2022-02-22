faaaaaf167a04f8bf6783253456dbbce8905d61e
Mahrud Sayrafi <mahrud@berkeley.edu>
2022-02-21 19:36:04 -0600

    normaliz: 3.9.2
    Closes #133.
    
    Signed-off-by: Mahrud Sayrafi <mahrud@umn.edu>

--- Formula/normaliz.rb
+++ Formula/normaliz.rb
@@ -1,10 +1,9 @@
 class Normaliz < Formula
   desc "Affine monoids, vector configurations, lattice polytopes, and rational cones"
   homepage "https://www.normaliz.uni-osnabrueck.de/"
-  url "https://github.com/Normaliz/Normaliz/releases/download/v3.9.1/normaliz-3.9.1.tar.gz"
-  sha256 "ad5dbecc3ca3991bcd7b18774ebe2b68dae12ccca33c813ab29891beb85daa20"
+  url "https://github.com/Normaliz/Normaliz/releases/download/v3.9.2/normaliz-3.9.2.tar.gz"
+  sha256 "434265281d4ac1a4e0c440403659a4ff92834508bb4f92c0d262387b44a18ee0"
   license "GPL-3.0-only"
-  revision 2
 
   bottle do
     root_url "https://github.com/Macaulay2/homebrew-tap/releases/download/normaliz-3.9.1_2"
