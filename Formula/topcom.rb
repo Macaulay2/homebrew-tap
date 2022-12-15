54968d2a009612dc01fcfff2d42abf7a53c670ec
Mahrud Sayrafi <mahrud@umn.edu>
2022-12-15 16:35:27 +0000

    topcom: update 1.1.2_5 bottle.

--- Formula/topcom.rb
+++ Formula/topcom.rb
@@ -8,12 +8,11 @@ class Topcom < Formula
   revision 5
 
   bottle do
-    root_url "https://github.com/Macaulay2/homebrew-tap/releases/download/topcom-0.17.8_5"
-    sha256 cellar: :any,                 arm64_monterey: "6284b48a829e7c9266520814d951fe69631bfb2d9871bfc697cf9710fa853b5b"
-    sha256 cellar: :any,                 big_sur:        "a46f2f3d4d13998c1abcee1d69c15b96b47081f76cb3da04c17e94c2130be07c"
-    sha256 cellar: :any,                 catalina:       "c98f77e405299b8b5376b5f00c8a5c6a8ebc605ae2131931cbb8520e08f2a422"
-    sha256 cellar: :any,                 monterey:       "ded3c406caba52d0c83114cf14688433df3225f709a7ed941f09b955983ae9a8"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "dd61807b16a01abbba5b7dd9a656263f5624d1d9f39a3b59c4ea7aa8550f33fc"
+    root_url "https://github.com/Macaulay2/homebrew-tap/releases/download/topcom-1.1.2_5"
+    sha256 cellar: :any, monterey:     "efab76d0cad36aa7c24e4c9024ae0e897a4727b16b04017b36261db569ac8664"
+    sha256 cellar: :any, big_sur:      "32499f758f1aad40512fb7a15a0ad6699bc3fea9cfede7a4a13af96cd23acc96"
+    sha256 cellar: :any, catalina:     "33861026c46157d77ca7989ef705385586724b0f1ab923dc0039ad2a5054a824"
+    sha256               x86_64_linux: "dc4297dd06d343181355aafcb5dfe2ceb8e51382852e9cde8a66adbea3698b0a"
   end
 
   depends_on "autoconf" => :build
