568d594efd591cb46afc9618147caf9e96b18514
Mahrud Sayrafi <mahrud@berkeley.edu>
2023-03-14 00:11:37 -0500

    topcom: revision bump
    Closes #177.
    
    Signed-off-by: Mahrud Sayrafi <mahrud@umn.edu>

--- Formula/topcom.rb
+++ Formula/topcom.rb
@@ -1,11 +1,10 @@
 class Topcom < Formula
   desc "Triangulations of point configurations and oriented matroids"
   homepage "https://www.wm.uni-bayreuth.de/de/team/rambau_joerg/TOPCOM/index.html"
-  # update
   url "https://www.wm.uni-bayreuth.de/de/team/rambau_joerg/TOPCOM-Downloads/TOPCOM-1_1_2.tgz"
   sha256 "4fb10754ee5b76056441fea98f2c8dee5db6f2984d8c14283b49239ad4378ab6"
   license "GPL-2.0-only"
-  revision 5
+  revision 6
 
   bottle do
     root_url "https://github.com/Macaulay2/homebrew-tap/releases/download/topcom-1.1.2_5"
@@ -32,7 +31,7 @@ class Topcom < Formula
            "--prefix=#{prefix}",
            "CPPFLAGS=-I#{Formula["gmp"].include} -I#{Formula["cddlib"].include}/cddlib",
            "LDFLAGS=-L#{Formula["gmp"].lib} -L#{Formula["cddlib"].lib}"
-    system "make", "install"
+    system "make", "install-strip"
   end
 
   test do
