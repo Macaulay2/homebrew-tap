5bf873925770a04e21ee46c01a0bb2b549a437dc
Mahrud Sayrafi <mahrud@umn.edu>
2024-03-01 05:15:19 +0000

    mathicgb: update 1.0_9 bottle.

--- Formula/mathicgb.rb
+++ Formula/mathicgb.rb
@@ -7,9 +7,11 @@ class Mathicgb < Formula
   revision 9
 
   bottle do
-    root_url "https://github.com/Macaulay2/homebrew-tap/releases/download/mathicgb-1.0_8"
-    sha256 cellar: :any,                 catalina:     "b85bd14a3c2f3a23b1bee738266cede6bbb1327e325b7a6333b18ff8f80146ed"
-    sha256 cellar: :any_skip_relocation, x86_64_linux: "81c1808ad174bd8399b6184b6ee1a7a0fdb00758ebc3ef316f8c06221cd72d3b"
+    root_url "https://github.com/Macaulay2/homebrew-tap/releases/download/mathicgb-1.0_9"
+    sha256 cellar: :any,                 arm64_sonoma: "0070f02a1163c88397bc27a9e619b4a99cb6e8b07ccebb63e84b307d04fe209e"
+    sha256 cellar: :any,                 ventura:      "238ec77aa4623193376ccbe2ca52eab54eec7498f0dbe9d518f4f1401889c1c1"
+    sha256 cellar: :any,                 monterey:     "4e157a8ddc5f1ff704ec857e3b7a6783000cee08824f3e86115d966c70a4fa4f"
+    sha256 cellar: :any_skip_relocation, x86_64_linux: "d3b6c0ea54f61c4aba3ce668a931781607edaf93913467f96814f641a901cb9b"
   end
 
   option "without-mgb", "don't build mgb"
