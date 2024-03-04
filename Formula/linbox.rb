9aeb63bd68d81b407c7939c39b01a5294b0cdeab
Mahrud Sayrafi <mahrud@berkeley.edu>
2024-03-04 10:51:58 -0800

    linbox: revision bump

--- Formula/linbox.rb
+++ Formula/linbox.rb
@@ -4,6 +4,7 @@ class Linbox < Formula
   url "https://github.com/linbox-team/linbox/releases/download/v1.7.0/linbox-1.7.0.tar.gz"
   sha256 "6d2159fd395be0298362dd37f6c696676237bc8e2757341fbc46520e3b466bcc"
   license "LGPL-2.1-or-later"
+  revision 1
 
   head "https://github.com/linbox-team/linbox.git", using: :git, branch: "master"
 
@@ -22,9 +23,11 @@ class Linbox < Formula
   depends_on "pkg-config" => :build
 
   depends_on "fflas-ffpack"
+  depends_on "flint"
   depends_on "givaro"
   depends_on "gmp"
   depends_on "libomp" if OS.mac?
+  depends_on "ntl"
   depends_on "openblas" unless OS.mac?
 
   def install
