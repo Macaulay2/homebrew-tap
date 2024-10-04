2dd75426d475aac93eae7498b5d8c18954330ed1
Mahrud Sayrafi <mahrud@berkeley.edu>
2024-10-05 00:24:51 +0200

    build macos-15 bottle for fflas-ffpack.rb

--- Formula/fflas-ffpack.rb
+++ Formula/fflas-ffpack.rb
@@ -9,7 +9,6 @@ class FflasFfpack < Formula
 
   bottle do
     root_url "https://github.com/Macaulay2/homebrew-tap/releases/download/fflas-ffpack-2.5.0"
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:   "01c93e2f074021463d97694bb0876122ce8e798eaaa862d09268f713e0a44105"
     sha256 cellar: :any_skip_relocation, arm64_monterey: "09243154036963bfe61d886796bafca1b124412a573ae8cc07c881aa2eb76d32"
     sha256 cellar: :any_skip_relocation, big_sur:        "77cb085376d5e5f5ca967cbc804a4e02e2858b002faf00635a4a3fea8231ad3c"
     sha256 cellar: :any_skip_relocation, catalina:       "784c162c6957906919ef2ba71aabc5ca3a657cb3a0df8b4057d101d377cee5b2"
