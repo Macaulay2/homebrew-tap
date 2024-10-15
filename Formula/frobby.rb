1eedcbda8a706544684b199db67bac0b09159474
Mahrud Sayrafi <mahrud@umn.edu>
2024-10-15 02:57:39 +0000

    frobby: update 0.9.5_4 bottle.

--- Formula/frobby.rb
+++ Formula/frobby.rb
@@ -7,13 +7,12 @@ class Frobby < Formula
   revision 4
 
   bottle do
-    root_url "https://github.com/Macaulay2/homebrew-tap/releases/download/frobby-0.9.5_4"
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "0ecec47d673eef61a73082772f3a383ee99c2b186e1c99a16c0b245fdfc87f0d"
-    sha256 cellar: :any_skip_relocation, big_sur:        "a771081b280379145c3656002582d82a2278f73f5492d672ce0e76d4255a9640"
-    sha256 cellar: :any_skip_relocation, catalina:       "4400a38be4024e11d76dbb40488f66d062befba6b0f24653436b2b33549a0028"
-    sha256 cellar: :any_skip_relocation, monterey:       "de11dfa58b111f3a1cc2e6cf4e9ab387d56356c68920ca727800cc1460295782"
-    sha256 cellar: :any_skip_relocation, ventura:        "b18b7097fbee369c2a79aef35b3560a56bfc2835fba699427f3d294a29774911"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "2b283abcb0fe6cef36f2f8cbb812da4c111e4f7c185d64092cbbd32306583a8f"
+    root_url "https://ghcr.io/v2/macaulay2/tap"
+    rebuild 1
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "e6e8560b7122d1d5b6b96207bc542f3ea391676c038a7a1b1486b5223c07a807"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "7a189732e48128ae8d92c4a996aa2a57003d38397a1d8bcb04319e449d997d07"
+    sha256 cellar: :any_skip_relocation, ventura:       "143537606638b12a10e7a4d1209b5d02003274e94030ea2c30076991b50e59ef"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "bca11b89ae883dbe4dda6b307449d0605d5abe5976814d77cda8db7a97ca37da"
   end
 
   unless OS.mac?
