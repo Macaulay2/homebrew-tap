b5059d6b984e739513a5f55557306ef66478bcc9
Mahrud Sayrafi <mahrud@berkeley.edu>
2024-10-20 19:45:14 +0200

    4ti2: 1.6.10

--- Formula/fourtitwo.rb
+++ Formula/fourtitwo.rb
@@ -1,21 +1,9 @@
 class Fourtitwo < Formula
   desc "Software for algebraic, geometric and combinatorial problems on linear spaces"
   homepage "https://4ti2.github.io/"
-  url "https://github.com/4ti2/4ti2/releases/download/Release_1_6_9/4ti2-1.6.9.tar.gz"
-  sha256 "3053e7467b5585ad852f6a56e78e28352653943e7249ad5e5174d4744d174966"
+  url "https://github.com/4ti2/4ti2/archive/refs/tags/Release_1_6_10.tar.gz"
+  sha256 "2f1bce3203da65b651d68cbd0ace0f89a16d1f436cf5f24e22bc15ec22df936a"
   license "GPL-2.0-or-later"
-  revision 1
-
-  bottle do
-    root_url "https://github.com/Macaulay2/homebrew-tap/releases/download/fourtitwo-1.6.9_1"
-    sha256 cellar: :any,                 arm64_sonoma:   "e71c07975a3bc84cf985c0b14a04b8132c43ebb21fb05d5398eb54f79c9f43fe"
-    sha256 cellar: :any,                 arm64_monterey: "e03d318e30f74cb384b2abb62301b6ba6974bdfe95ef93484e3fc93cc80f4388"
-    sha256 cellar: :any,                 big_sur:        "ce0d7ed1e2aef4a358e2b3961969c79cd28898357517a550120279e853e5c368"
-    sha256 cellar: :any,                 catalina:       "d98a4cdf6356e5f1711969f1b06035004a16b265214dd84290797075b3457427"
-    sha256 cellar: :any,                 monterey:       "5578687a70da0b4333386aa94489c405c70ce53002aead36504f1de3805598d8"
-    sha256 cellar: :any,                 ventura:        "54f7fcef9c462e0efe29f03b2f62ed867529b07ecb10282b8e07e09902be8371"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "4ac3d558537776b89e453ff69b53009686b68afaf8b9e2be076c46c576f75ba2"
-  end
 
   depends_on "autoconf" => :build
   depends_on "automake" => :build
