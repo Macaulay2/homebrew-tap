583337e66dd956599b25393e63e3b71e61e164a1
Mahrud Sayrafi <mahrud@umn.edu>
2024-11-01 15:58:53 +0000

    macaulay2: add 1.24.11 bottle.

--- Formula/macaulay2.rb
+++ Formula/macaulay2.rb
@@ -8,6 +8,14 @@ class Macaulay2 < Formula
 
   head "https://github.com/Macaulay2/M2/archive/refs/heads/development.tar.gz"
 
+  bottle do
+    root_url "https://ghcr.io/v2/macaulay2/tap"
+    sha256 cellar: :any, arm64_sequoia: "b7f1d938803109c5307bc3c13ff02b9311aae3d49ac83178f2c8b1c5e4f8df16"
+    sha256 cellar: :any, arm64_sonoma:  "0b80901a9b6d53e28fb9647501a714129ef2edc1549d5e6880a1aaa20dc44ef8"
+    sha256 cellar: :any, ventura:       "32da7633b1cc5de66269687ce9b2abf15d5f95adb2a08b8d7c0ec4ea6b9351f0"
+    sha256               x86_64_linux:  "8c326857eefc779389f439208330fa5e33fcaceb1770ccd3d71b67dd908e64c2"
+  end
+
   depends_on "bison" => :build
   depends_on "cmake" => :build
   depends_on "ninja" => :build
