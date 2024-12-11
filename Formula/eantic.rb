940c450f6eb16f308e7841baa31752ff82c63259
Mahrud Sayrafi <mahrud@berkeley.edu>
2024-12-11 01:17:36 +0100

    fixed the e-antic homepage

--- Formula/eantic.rb
+++ Formula/eantic.rb
@@ -1,6 +1,6 @@
 class Eantic < Formula
   desc "Computing with Real Embedded Number Fields"
-  homepage "https://flatsurf.github.io/e-antic/libeantic/"
+  homepage "https://flatsurf.github.io/e-antic/"
   url "https://github.com/flatsurf/e-antic/releases/download/2.0.2/e-antic-2.0.2.tar.gz"
   sha256 "8328e6490129dfec7f4aa478ebd54dc07686bd5e5e7f5f30dcf20c0f11b67f60"
   license any_of: ["LGPL-3.0-only", "GPL-3.0-only"]
