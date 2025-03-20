e7a591c6aad9c68d5c5e19ef4be0fa5a858d21b7
Mahrud Sayrafi <mahrud@berkeley.edu>
2025-03-20 05:51:55 -0400

    rebuild linbox

--- Formula/linbox.rb
+++ Formula/linbox.rb
@@ -8,14 +8,6 @@ class Linbox < Formula
 
   head "https://github.com/linbox-team/linbox.git", using: :git, branch: "master"
 
-  bottle do
-    root_url "https://github.com/Macaulay2/homebrew-tap/releases/download/linbox-1.7.0_1"
-    sha256 cellar: :any,                 arm64_sonoma: "e2efdb9d1ed680e5756ae9e3162d208124ffcbc471f3c60abbf4aa93437ed62b"
-    sha256 cellar: :any,                 ventura:      "aa3efe72c0995118689e101c14f485bdf73ba5e8f4c2c52810d1e447d8ef4c62"
-    sha256 cellar: :any,                 monterey:     "ecefbd6696dc3233eead7b8bf864d577e2938b8894494d2ebe1224092c6cd5f7"
-    sha256 cellar: :any_skip_relocation, x86_64_linux: "dc2c6b45edadc5ef7fae8ba064c018d3ac02a2849c0cac61504dd25b3fb2cc2e"
-  end
-
   depends_on "autoconf" => :build
   depends_on "automake" => :build
   depends_on "libtool" => :build
