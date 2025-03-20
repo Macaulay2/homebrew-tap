d921e159172f6e911a5f047e965d4348e07c58cd
Mahrud Sayrafi <mahrud@berkeley.edu>
2025-03-20 05:51:41 -0400

    patched e-antic for flint 3.2

--- Formula/eantic.rb
+++ Formula/eantic.rb
@@ -5,21 +5,17 @@ class Eantic < Formula
   sha256 "8328e6490129dfec7f4aa478ebd54dc07686bd5e5e7f5f30dcf20c0f11b67f60"
   license any_of: ["LGPL-3.0-only", "GPL-3.0-only"]
 
-  bottle do
-    root_url "https://ghcr.io/v2/macaulay2/tap"
-    rebuild 1
-    sha256 cellar: :any_skip_relocation, arm64_sequoia: "d0e8ef3fe37854d58fb75c7ed58ee7549b4972253fb829306f160c7ef28c8868"
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "de2ad94f769b55b1ba12dbb3ac3ffd4eb666d1a2b8e51d29d518ec7cc1eca146"
-    sha256 cellar: :any_skip_relocation, ventura:       "3005bb2e581a81a157e9f18ee88e3e5f61002e5a3f545ae6ce01b037f45ab85f"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "08733bf692c6201c1dee197cf070bf4eb17a05aaff361681caaee70cdf6adf7f"
-  end
-
   depends_on "autoconf" => :build
   depends_on "automake" => :build
   depends_on "libtool" => :build
 
   depends_on "boost"
   depends_on "flint"
+
+  patch do
+    url "https://github.com/flatsurf/e-antic/commit/66ecdffd7e83611e1c3df5049634d88f089b0104.patch?full_index=1"
+    sha256 "bdfbb3b846017bee68a53a13c974e76958f5aa5c0a50929dc3957560d616f1ec"
+  end
 
   def install
     ENV.cxx11
