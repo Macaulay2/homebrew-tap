782ee16d67e555565b593656f7416563507e731f
Mahrud Sayrafi <mahrud@umn.edu>
2025-04-01 22:12:53 +0000

    factory: add 4.4.1 bottle.

--- Formula/factory.rb
+++ Formula/factory.rb
@@ -5,6 +5,15 @@ class Factory < Formula
   sha256 "345ec8ab2481135d18244e2a2ff6bc16e812a39a9eb5ac5d578956d8e0526e6e"
   license any_of: ["GPL-2.0-only", "GPL-3.0-only"]
 
+  bottle do
+    root_url "https://ghcr.io/v2/macaulay2/tap"
+    rebuild 1
+    sha256 cellar: :any,                 arm64_sequoia: "88b69f38fd87fac2ee9d503b77ee56544d8a6cdc7cb0b86b2fbce461f7f213ef"
+    sha256 cellar: :any,                 arm64_sonoma:  "a0642620c8a97b6c11436a273746babb1e1f503ae380432846436eb1dd7e4af2"
+    sha256 cellar: :any,                 ventura:       "5b4f96f9daf9500deb9d75cbe2c3759e4f90647aeced7b3cd4f37ee75ce4498c"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "0d6f10ab822d3d93ddc18fa540ce415d9634a52655f4fbfbe3d3cf596f740584"
+  end
+
   keg_only "it conflicts with singular"
 
   depends_on "autoconf" => :build
