dd0a7ca41d4ffadce78624e0dd339ab09590bf44
Mahrud Sayrafi <mahrud@berkeley.edu>
2025-04-01 17:49:40 -0400

    rebuild bottles dependent on Flint
    Closes #267.
    
    Signed-off-by: Mahrud Sayrafi <mahrud@umn.edu>

--- Formula/eantic.rb
+++ Formula/eantic.rb
@@ -5,15 +5,6 @@ class Eantic < Formula
   sha256 "8328e6490129dfec7f4aa478ebd54dc07686bd5e5e7f5f30dcf20c0f11b67f60"
   license any_of: ["LGPL-3.0-only", "GPL-3.0-only"]
 
-  bottle do
-    root_url "https://ghcr.io/v2/macaulay2/tap"
-    rebuild 2
-    sha256 cellar: :any_skip_relocation, arm64_sequoia: "246b27f9587f14aa111990b764ad2ea9ea961fb1ca533e50b19bfb963ef15b65"
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "e14fe994b3741b36de0de5b0f132488eb8efa168372ccade89de9c1ba820d9ce"
-    sha256 cellar: :any_skip_relocation, ventura:       "f10795213159b95510676d52f369bbcdbd743e2c26c5007b4400a653b2f7f5eb"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "06733ad375399cb41af1390534f9936e4b9edca342f6ca50b05c9158965ac584"
-  end
-
   depends_on "autoconf" => :build
   depends_on "automake" => :build
   depends_on "libtool" => :build
