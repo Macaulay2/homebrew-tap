b751cbfa6c089bc7646cc9a67f7c8922c38141f6
Mahrud Sayrafi <mahrud@berkeley.edu>
2025-06-16 14:48:01 -0400

    bump e-antic revision for flint 3.3.1

--- Formula/eantic.rb
+++ Formula/eantic.rb
@@ -4,15 +4,7 @@ class Eantic < Formula
   url "https://github.com/flatsurf/e-antic/releases/download/2.0.2/e-antic-2.0.2.tar.gz"
   sha256 "8328e6490129dfec7f4aa478ebd54dc07686bd5e5e7f5f30dcf20c0f11b67f60"
   license any_of: ["LGPL-3.0-only", "GPL-3.0-only"]
-
-  bottle do
-    root_url "https://ghcr.io/v2/macaulay2/tap"
-    rebuild 3
-    sha256 cellar: :any_skip_relocation, arm64_sequoia: "6cbbd074435c91b1b4e3159c6b9214488832302cbf58fe0d48325324ef1191e5"
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "54e6ca2e11c43f3ec25d0d9b406f76ab796ddc619e82c4227d792eba6853d42a"
-    sha256 cellar: :any_skip_relocation, ventura:       "60079eead5875bae38c96ccb5fbac8261073b36763bb46b4623bf5192e19bf6a"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "d5c54e1a3a8090ea2fbee2f341074a82ccbfd749ae19455cf87380e44bd167ca"
-  end
+  revision 1
 
   depends_on "autoconf" => :build
   depends_on "automake" => :build
