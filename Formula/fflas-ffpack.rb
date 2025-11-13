28a44e35f8a14a63fda7f14bffe0cb3ce8cd34f3
Doug Torrance <dtorrance@piedmont.edu>
2025-11-13 11:51:29 -0500

    Build fflas-ffpack bottle for tahoe
    Also add git branch for head option for "brew audit"
    
    Closes #304.
    
    Signed-off-by: Doug Torrance <dtorrance@piedmont.edu>

--- Formula/fflas-ffpack.rb
+++ Formula/fflas-ffpack.rb
@@ -5,16 +5,7 @@ class FflasFfpack < Formula
   sha256 "dafb4c0835824d28e4f823748579be6e4c8889c9570c6ce9cce1e186c3ebbb23"
   license "LGPL-2.1-or-later"
 
-  head "https://github.com/linbox-team/fflas-ffpack.git", using: :git
-
-  bottle do
-    root_url "https://ghcr.io/v2/macaulay2/tap"
-    rebuild 1
-    sha256 cellar: :any_skip_relocation, arm64_sequoia: "08c831d63ba6d67a0bc3e131ed7967a052833dfd2cb0f4d3afd432ef3deb6186"
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "832b9cea1e610d857d35c01a432a2c81b2523dca75ca66aa6927e748d0d4176d"
-    sha256 cellar: :any_skip_relocation, ventura:       "886cee68ccab849527a2357cea74081d2b94d733541e2f47d393dca21d54b079"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "2b9ab0804e231214eeb5639a82f876dc96575f8f983cd4d2a0a17026a5d9d45d"
-  end
+  head "https://github.com/linbox-team/fflas-ffpack.git", using: :git, branch: "master"
 
   depends_on "autoconf" => :build
   depends_on "automake" => :build
