fca851871a8b98c2caaf90d7e621f62c2df051c0
Doug Torrance <dtorrance@piedmont.edu>
2025-11-13 17:45:11 +0000

    topcom: add 1.1.2_6 bottle.

--- Formula/topcom.rb
+++ Formula/topcom.rb
@@ -6,6 +6,15 @@ class Topcom < Formula
   license "GPL-2.0-only"
   revision 6
 
+  bottle do
+    root_url "https://ghcr.io/v2/macaulay2/tap"
+    rebuild 2
+    sha256 cellar: :any,                 arm64_tahoe:   "35832af090797163c088942ced3c27b987ff085b5807ee79587261e6485571ea"
+    sha256 cellar: :any,                 arm64_sequoia: "5aa8c1b2b0639980cb70a12822912d45f0db39daa90c17fb05aa0e03807306f6"
+    sha256 cellar: :any,                 arm64_sonoma:  "2a8884f32f3a1c50f5a2a664b840418083d9e1af7e1f0fff19eab1f442df5daa"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "07b73acda37b0b7d21a1fa621559688d462e5e2566de31ec98ceab1eaf49089c"
+  end
+
   depends_on "autoconf" => :build
   depends_on "automake" => :build
   depends_on "libtool" => :build
