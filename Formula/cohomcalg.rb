2596e95b93e64974a4db8aaaa58635bbd55a5a6e
Doug Torrance <dtorrance@piedmont.edu>
2025-11-13 10:53:28 -0500

    Build tahoe bottles for factory and cohomcalg

--- Formula/cohomcalg.rb
+++ Formula/cohomcalg.rb
@@ -6,16 +6,6 @@ class Cohomcalg < Formula
   license "GPL-3.0-only"
   revision 1
 
-  bottle do
-    root_url "https://ghcr.io/v2/macaulay2/tap"
-    rebuild 1
-    sha256 cellar: :any_skip_relocation, arm64_sequoia: "0494687d5bc2bf2c3552ed16c9fd9709e49a7475b8271528dc6428555014e585"
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "d6b7c119bed3c521348f1b828bf317af6e99a2c207b0edeac17ec3048a894712"
-    sha256 cellar: :any_skip_relocation, ventura:       "f0789206c6198b4d49aa2ed74f34f6b4f56721c58f9f99db18ec16e644961f1b"
-    sha256 cellar: :any_skip_relocation, monterey:      "72b8c0c3b24a56892bb4ac78921bb03b96114584c213e93f9602353917e4055c"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "4deafd4a90a2d011690e5c097fe3a6deb27736237a33007ad7bbaa09c992bdb8"
-  end
-
   def install
     ENV.cxx11
     system "make", "prefix=#{prefix}",
