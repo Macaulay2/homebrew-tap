c6baf962c517548fa1476efef5bbfe997c3f053e
Doug Torrance <dtorrance@piedmont.edu>
2025-11-13 17:45:11 +0000

    gfan: add 0.7 bottle.

--- Formula/gfan.rb
+++ Formula/gfan.rb
@@ -5,6 +5,15 @@ class Gfan < Formula
   sha256 "ab833757e1e4d4a98662f4aa691394013ea9a226f6416b8f8565356d6fcc989e"
   license "GPL-2.0-or-later"
 
+  bottle do
+    root_url "https://ghcr.io/v2/macaulay2/tap"
+    rebuild 1
+    sha256 cellar: :any, arm64_tahoe:   "b7d9ec3054061ccb64f3a2d443e4813d7b62ef5c548a391620e5b4e35a1c56ff"
+    sha256 cellar: :any, arm64_sequoia: "16264ac7f91bb5a8a7d7fcaa200a070c735e824e312f56c792c68457627f1098"
+    sha256 cellar: :any, arm64_sonoma:  "1d14f34e47676a30a271dcd398b204f8e5eda67dedbbb9b56f97db978bd89ec8"
+    sha256               x86_64_linux:  "8b0f30a69cf0fa2516a2bba00d2d6c0d76abeb8256bc932622d7ffc7332b0b26"
+  end
+
   depends_on "gcc@14" => :build
 
   depends_on "cddlib"
