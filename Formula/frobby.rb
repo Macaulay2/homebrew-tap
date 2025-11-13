5d825df87e7d92e509fe1a4f1a208d3b1d682f8b
Doug Torrance <dtorrance@piedmont.edu>
2025-11-13 09:12:36 -0500

    Bump frobby 0.9.5 revision to 5 (build tahoe bottle)
    Also update homepage (original website has disappeared)
    
    Closes #302.
    
    Signed-off-by: Doug Torrance <dtorrance@piedmont.edu>

--- Formula/frobby.rb
+++ Formula/frobby.rb
@@ -1,10 +1,10 @@
 class Frobby < Formula
   desc "Computations With Monomial Ideals"
-  homepage "https://www.broune.com/frobby/"
+  homepage "https://github.com/Macaulay2/frobby"
   url "https://github.com/Macaulay2/frobby.git", using: :git, branch: "master"
   version "0.9.5"
   license "GPL-2.0-only"
-  revision 4
+  revision 5
 
   bottle do
     root_url "https://ghcr.io/v2/macaulay2/tap"
