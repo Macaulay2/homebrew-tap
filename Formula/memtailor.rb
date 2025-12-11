16fd2277b914c2ba3c2c72657a73d0a7c607e693
Doug Torrance <dtorrance@piedmont.edu>
2025-12-11 22:37:51 +0000

    memtailor: update 1.1 bottle.

--- Formula/memtailor.rb
+++ Formula/memtailor.rb
@@ -6,11 +6,11 @@ class Memtailor < Formula
   license "BSD-3-Clause"
 
   bottle do
-    root_url "https://github.com/Macaulay2/homebrew-tap/releases/download/memtailor-1.0_10"
-    sha256 cellar: :any_skip_relocation, arm64_sonoma: "8625ebc5b505c45a6791b9e37135f77e9fb5581c872dc67e428de90a8267ab72"
-    sha256 cellar: :any_skip_relocation, ventura:      "f76bd8f10b3e75f57af082550556c1d775db0d3988caa758242749cba3d15edd"
-    sha256 cellar: :any_skip_relocation, monterey:     "17d26b8168d0b0e89957fc7980153603a8dcd7f9cdc2db36053321b51da4b00b"
-    sha256 cellar: :any_skip_relocation, x86_64_linux: "d6d00ef110f20f2452fdbac2a71ffefa02e221e37ab45ec23d736fd19b3014d4"
+    root_url "https://ghcr.io/v2/macaulay2/tap"
+    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "de32109134cbac1824dc294c436e219df07bcb5dfb3cd70a89fe9775289caeb0"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "94ee72f40221e584b1b7b80f56c1e78e349cfa11b8a4f10e40e34a96ebb39586"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "fa681c0b63cfd59ac2354833db8a8790fbc28a83505df281d3cc4b475155e762"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "813ba63fa3246911d8db138c006a24fd71670ad42849d06a63af57d8ba572d2b"
   end
 
   unless OS.mac?
