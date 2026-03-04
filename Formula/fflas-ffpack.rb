69a067907f05ffcbdc836a5e30c5ec0575c53823
Dima Pasechnik <dima@pasechnik.info>
2026-03-03 23:02:34 -0600

    use the usual -L/-l syntax to link libomp

--- Formula/fflas-ffpack.rb
+++ Formula/fflas-ffpack.rb
@@ -32,7 +32,7 @@ class FflasFfpack < Formula
     ENV.cxx11
     if OS.mac?
       libomp = Formula["libomp"]
-      ENV["OMPFLAGS"] = "-Xpreprocessor -fopenmp -I#{libomp.opt_include} #{libomp.opt_lib}/libomp.dylib"
+      ENV["OMPFLAGS"] = "-Xpreprocessor -fopenmp -I#{libomp.opt_include} -L#{libomp.opt_lib} -lomp"
     else
       ENV["OMPFLAGS"] = "-fopenmp"
     end
