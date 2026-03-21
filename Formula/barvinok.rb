5595aae29fa5715ee23fea6d941263c658f2f142
Mahrud Sayrafi <3147718+mahrud@users.noreply.github.com>
2026-03-21 15:24:35 +0000

    barvinok: update 0.41.8-M2 bottle.

--- Formula/barvinok.rb
+++ Formula/barvinok.rb
@@ -7,10 +7,10 @@ class Barvinok < Formula
 
   bottle do
     root_url "https://ghcr.io/v2/macaulay2/tap"
-    sha256 cellar: :any,                 arm64_tahoe:   "a72bd3ef20974f50c8e3ac6db28954fdf0bc8d2e783fe8714775b2a710b0433f"
-    sha256 cellar: :any,                 arm64_sequoia: "9da64f902c1f70d79297c90c5b6130c23b1bfa3f665683adf13f099e863105fc"
-    sha256 cellar: :any,                 arm64_sonoma:  "68c3a890c3511994fd2414a5dc67e81b31a62568939796197a2926a22ea7cd2a"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "bc79f7aa88a8285455970139735120613346e19a25a1e3cc417a880e09e5b384"
+    sha256 cellar: :any,                 arm64_tahoe:   "03f684954e9ff6211a529df2bd9e388696b709b58f8d5c52e26a106662939ec3"
+    sha256 cellar: :any,                 arm64_sequoia: "c81cb6f2ac7953d94cc37e69434ed7a61f889a963006756cbbc479ffe5b01510"
+    sha256 cellar: :any,                 arm64_sonoma:  "74f8fc39e49246cc02d7015524826a88436bf1f1e85eb83a97899227d059823b"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "1d0d7b1a00e4ba87f16736dd997ae43380b5938a51c2d1d676d4f31680fc0c82"
   end
 
   depends_on "autoconf" => :build
